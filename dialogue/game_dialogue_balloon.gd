9c3b36c14e3bfc37df2b0cbc960c550ffb319d12
Mark Hewitt <13398084+mrhewitt@users.noreply.github.com>
2025-07-15 14:58:28 +0200

    Change character talking  in emotes panel

--- dialogue/game_dialogue_balloon.gd
+++ dialogue/game_dialogue_balloon.gd
@@ -2,13 +2,20 @@ extends BaseGameDialogueBalloon
 
 @onready var emotes_panel: Panel = $Balloon/Panel/MarginContainer/HBoxContainer/EmotesPanel
 
-
-func start(dialogue_resource: DialogueResource, title: String, extra_game_states: Array = []) -> void:
-	super.start(dialogue_resource, title, extra_game_states)
-	emotes_panel.play_talking_animation()
+func _ready() -> void:	
+	DialogueManager.got_dialogue.connect(on_dialog_line)
+	
+	
+func on_dialog_line(line: DialogueLine) -> void:
+	emotes_panel.play_talking_animation(line.character)
+	
+	
+#func start(dialogue_resource: DialogueResource, title: String, extra_game_states: Array = []) -> void:
+#	super.start(dialogue_resource, title, extra_game_states)
+#	emotes_panel.play_talking_animation()
 	
 
-func next(next_id: String) -> void:
-	super.next(next_id)
-	emotes_panel.play_talking_animation()
+#func next(next_id: String) -> void:
+#	super.next(next_id)
+#	emotes_panel.play_talking_animation()
 		
--- scenes/ui/emotes_panel.gd
+++ scenes/ui/emotes_panel.gd
@@ -10,14 +10,18 @@ func _ready() -> void:
 	play_emote(idle_emotes[0])
 	InventoryManager.inventory_updated.connect(on_inventory_changed)
 	GameDialogueManager.feel_the_animals.connect(on_feed_animals)
-	
-	
+
+
 func play_emote(animation: String) -> void:
 	animated_sprite_2d.play(animation)
 
 
-func play_talking_animation() -> void:
-	play_emote("emote_12_talking")
+func play_talking_animation(character: String) -> void:
+	var talking_animation: String = character.to_lower() + "_talking" 
+	if animated_sprite_2d.sprite_frames.has_animation(talking_animation):
+		play_emote(talking_animation)
+	else:
+		play_emote("emote_12_talking")
 
 
 func _on_emote_idle_timer_timeout() -> void:
@@ -31,3 +35,5 @@ func on_inventory_changed(collectable_name: String, collectable_count: int) -> v
 	
 func on_feed_animals() -> void:
 	play_emote("emote_6_love_kiss")	
+	
+	
